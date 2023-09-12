485c153b43995a7dd5e82b1596ae3c63df998e89
Jack Ellis <jackellis1504@gmail.com>
2023-09-12 16:44:24 +0100

    added soup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from requests import get
+import requests
+from bs4 import BeautifulSoup
 
+def get_api_player(id):
+  return requests.get(f"https://www.mut.gg/api/mutdb/player-items/{id}").json()
 
-def make_mut_gg_request(url = ''):
-  return get(f'https://www.mut.gg/{url}')
+def get_api_player_from_web_link(link):
+  split_link = link.split('/')
+  id = split_link[-1]
+  return get_api_player(id)
 
+vgm_url = 'https://www.mut.gg/players'
+html_text = requests.get(vgm_url).text
+soup = BeautifulSoup(html_text, 'html.parser')
 
+for link in soup.find_all('a', class_='player-list-item__link'):
+  player = get_api_player_from_web_link(link.get('href'))
+  first = player.get('firstName')
+  last = player.get('lastName')
+  print(f"{first} {last}")
 
-body = make_mut_gg_request('players')
-print(body.content)
