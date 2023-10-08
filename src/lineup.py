a50b650d4273c2ce4ddfa68d26dc89d1cff6d379
Jack Ellis <jackellis1504@gmail.com>
2023-10-08 14:58:37 +0100

    Sorting updates

--- src/lineup.py
+++ src/lineup.py
@@ -78,7 +78,7 @@ class Lineup:
         for position in self.get_positions().keys():
             for player in getattr(self, position):
                 ret_val[player.chem] = ret_val.get(player.chem, 0) + 1
-        sorted_ret_val = sorted(ret_val.items(), lambda x: x[1], reverse = True)
+        sorted_ret_val = sorted(ret_val.items(), key=lambda x: x[1], reverse=True)
         return dict(sorted_ret_val)
 
     def to_dict(self) -> dict:
