b877708651505c5c1f36137f1e0db95cef771cef
Jack Ellis <jackellis1504@gmail.com>
2023-11-15 21:30:39 +0000

    Updates to saving

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ with open("acceptable_teams.json") as jsonTeams:
     acceptable_teams_list = json.load(jsonTeams)
     for acceptable_team_list in acceptable_teams_list:
         if len(acceptable_team_list) > 0:
-            slugged_teams = "-".join(acceptable_team_list)
+            slugged_teams = "_".join(acceptable_team_list)
         else:
             slugged_teams = "all"
         print(slugged_teams)
         lineup = Lineup.get_lineup(acceptable_teams=acceptable_team_list)
         lineup.make_best()
-        with open(output_dir(f"{slugged_teams}-lineup.json"), "w") as lineup_file:
-            lineup_dict = lineup.to_dict()
-            json.dump(lineup_dict, lineup_file, indent=4)
-        lineup.to_csv(output_dir(f"{slugged_teams}-lineup.csv"))
+        lineup.to_csv(output_dir(f"{slugged_teams}.csv"))
